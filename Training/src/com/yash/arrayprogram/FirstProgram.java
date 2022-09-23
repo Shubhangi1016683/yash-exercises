539a33a4cfdfd8fd833eb54baa3233adb51f431b
Shubhangi1016683 <114235188+Shubhangi1016683@users.noreply.github.com>
2022-09-23 19:01:22 +0530

    Add files via upload

---
+++ Training/src/com/yash/arrayprogram/FirstProgram.java
@@ -0,0 +1,35 @@
+package com.yash.arrayprogram;
+
+/*WAP to find third minimum number from list of numbers*/
+import java.util.Scanner;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter array size");
+		int size=sc.nextInt();
+		int a[]=new int[size];
+		System.out.println("Enter array elemnts");
+		for(int i=0;i<size;i++) {
+			a[i]=sc.nextInt();
+		}
+		int temp;
+		for(int i=0;i<size;i++) {
+			for(int j=i+1;j<size;j++) {
+			if(a[i]>a[j]) {
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+			
+		}
+		int i;
+		for( i=0;i<size;i++) {
+			System.out.println(a[i]);
+		}
+		System.out.println("third minimum number :"+a[2]);
+	}
+
+}
