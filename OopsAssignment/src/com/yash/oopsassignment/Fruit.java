6029cd44876b5904ed49d518af126ea5f4265e57
Shubhangi1016683 <114235188+Shubhangi1016683@users.noreply.github.com>
2022-10-04 17:33:22 +0530

    Add files via upload

---
+++ OopsAssignment/src/com/yash/oopsassignment/Fruit.java
@@ -0,0 +1,12 @@
+package com.yash.oopsassignment;
+
+public class Fruit {
+	String fruitName;
+	String  fruitColor;
+	public Fruit(String fruitName, String fruitColor) {
+		super();
+		this.fruitName = fruitName;
+		this.fruitColor = fruitColor;
+	}
+	
+}
